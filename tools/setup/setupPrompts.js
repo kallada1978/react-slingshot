be723db3cda59668dd681fe640784458989ce54f
Santhosh <santhosh.tr@gmail.com>
2017-09-07 15:29:02 -0700

    minor

--- tools/setup/setupPrompts.js
+++ tools/setup/setupPrompts.js
@@ -13,17 +13,16 @@ module.exports = [
     description: 'Version (default: 0.1.0)'
   }, 
   {
-    name: 'email',
-    description: 'Email',
-    pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    ,
-    message: 'Email field is manadatory'
-  },
-  {
     name: 'author',
     description: 'Author',
     pattern: /^[^.*\\S+.* ][a-z0-9-_~]+$/,
     message: 'Author field is manadatory' 
+  },
+  {
+    name: 'email',
+    description: 'Email',
+    pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    message: 'Email field is manadatory'
   },
   {
     name: 'license',
